afee3621b8dbd5d053e425544e74499aeff9cef2
ogiovanni <oscargiovanni@gmail.com>
2025-06-11 22:49:00 -0500

    Añade la consulta para obtener un curso específico por ID y actualiza los niveles de curso en el esquema GraphQL.

--- academia-online/src/resolvers/query.ts
+++ academia-online/src/resolvers/query.ts
@@ -2,13 +2,13 @@ import { IResolvers } from "@graphql-tools/utils";
 import { database } from "../data/data.store";
 
 
-const query : IResolvers = {
+const query: IResolvers = {
     Query: {
         estudiantes(): any {
             return database.estudiantes;
         },
         estudiante(__: void, { id }): any {
-            const resultado = database.estudiantes.filter(estudiante => estudiante.id === id) [0];
+            const resultado = database.estudiantes.filter(estudiante => estudiante.id === id)[0];
             if (resultado === undefined) {
                 return {
                     id: '-1',
@@ -21,8 +21,27 @@ const query : IResolvers = {
         },
         cursos(): any {
             return database.cursos;
+        },
+        curso(__: void, { curso }): any {
+            const resultado = database.cursos.filter(curso_ => curso_.id === curso)[0];
+            if (resultado === undefined) {
+                return {
+                    id: '-1',
+                    title: `No se ha encontrado el curso con el ID ${curso}`,
+                    description: '',
+                    clases: -1,
+                    time: 0.0,
+                    logo: '',
+                    level: 'ALL',
+                    path: '',
+                    teacher: '',
+                    reviews: []
+                }
+            }
+            return resultado;
         }
     }
 }
+
 
 export default query;
